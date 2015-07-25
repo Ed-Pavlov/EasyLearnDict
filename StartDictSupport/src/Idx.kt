53eb1311aacbb4907b489faecf8425701a63e390
Ed Pavlov <ed.pavlov@gmail.com>
2015-07-25 13:11:45 +0200

    Use postfix tree for keeping dictionary index in memory

--- StartDictSupport/src/Idx.kt
+++ StartDictSupport/src/Idx.kt
@@ -1,12 +1,15 @@
+import collection.PatriciaTrie
+import collection.StringKeyAnalyzer
+import collection.Trie
 import java.io.DataInputStream
 import java.io.File
 import java.util.*
 
-fun createIdx(filePath : String, ifo : Ifo) : HashMap<String, Pair<Long, Long>>{filePath
+fun createIndex(filePath : String, ifo : Ifo) : PatriciaTrie<String, Pair<Long, Long>> {
   val file = File(filePath)
   val stream = DataInputStream(file.inputStream())
 
-  val map = HashMap<String, Pair<Long, Long>>()
+  val index = PatriciaTrie<String, Pair<Long, Long>>(StringKeyAnalyzer.BYTE)
   while(stream.available() > 0){
     val buffer = ByteArray(257)
     var byte : Byte;
@@ -15,14 +18,11 @@ fun createIdx(filePath : String, ifo : Ifo) : HashMap<String, Pair<Long, Long>>{
       byte = stream.readByte()
       buffer[i++] = byte
     } while(byte.compareTo(0) != 0)
-//    byte = stream.readByte()
 
-    val word = String(buffer, 0, i - 1).toLowerCase()
+    val word = String(buffer, 0, i - 1)
 
     val wordOffset = if (ifo.idxOffsetBits == BitCount.x64) stream.readLong() else stream.readInt().toLong()
-    map[word] = Pair(wordOffset, stream.readInt().toLong())
+    index.put(word, Pair(wordOffset, stream.readInt().toLong()))
   }
-  return map;
+  return index
 }
-
-data class Idx(val word : String, val offset : Long, val size : Long )
--- StartDictSupportTest/src/CreateIndexTest.kt
+++ StartDictSupportTest/src/CreateIndexTest.kt
@@ -1,13 +1,54 @@
+import collection.Cursor
+import collection.PatriciaTrie
+import collection.StringKeyAnalyzer
+import org.junit.Assert
 import org.junit.Test
+import java.io.DataInputStream
+import java.io.File
+import org.hamcrest.core.Is
+import java.util.*
 
 class CreateIndexTest{
-  Test
-  fun MemoryTest(){
-    val name = "data\\BeerDeRu"
-    val ifo = Ifo.create(name + ".ifo")
+  val name = "data\\UniversalDeRu"
 
+  Test
+  fun IndexShouldReturnRightData(){
+    // --arrange
+    val ifo = Ifo.create(name + ".ifo")
     val usedMemory = memory.usedMemory
-    val index = createIdx(name + ".idx", ifo)
-    println(memory.usedMemory - usedMemory)
+
+    // --act
+    val trie = createIndex(name + ".idx", ifo)
+    println("Memory used by index: " + (memory.usedMemory - usedMemory))
+
+    // --assert
+    val index = createExpected(name + ".idx", ifo)
+        for((word, expected) in index)
+          Assert.assertThat(word, trie.get(word), Is.`is`(expected))
+  }
+
+  private fun createExpected(filePath : String, ifo : Ifo) : HashMap<String, Pair<Long, Long>> {
+    val file = File(filePath)
+    val stream = DataInputStream(file.inputStream())
+
+    val map = HashMap<String, Pair<Long, Long>>()
+    while(stream.available() > 0){
+      val buffer = ByteArray(257)
+      var byte : Byte;
+      var i = 0
+      do{
+        byte = stream.readByte()
+        buffer[i++] = byte
+      } while(byte.compareTo(0) != 0)
+      //    byte = stream.readByte()
+
+      val word = String(buffer, 0, i - 1)
+
+      val wordOffset = if (ifo.idxOffsetBits == BitCount.x64) stream.readLong() else stream.readInt().toLong()
+      if(map.containsKey(word))
+        throw Exception(word)
+      map[word] = Pair(wordOffset, stream.readInt().toLong())
+    }
+    return map;
   }
 }
